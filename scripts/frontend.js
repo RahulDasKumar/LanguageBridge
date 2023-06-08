a63626bb54948f502785070ba1747528e25a0bc1
RahulDasKumar <dasrkd3@gmail.com>
2023-06-08 15:43:41 -0400

    fixed ports disconnecting

--- scripts/frontend.js
+++ scripts/frontend.js
@@ -1,49 +1,64 @@
-
-/*
-add event listener that fires every time you click on a youtube comment
-When clicked on a div that has a id named "comment-comment" 
-it will extract the text content
-*/
-
-//get the event.targetinnerHTML
-// filter out the comment by looking at thE event.target
-// it should equal yt-formatted-string#content-text.style-scope.ytd-comment-renderer
-
-//open port named translate
-
-const array = ['style-scope ytd-comment-renderer','style-scope yt-formatted-string',]
+const array = ['style-scope ytd-comment-renderer', 'style-scope yt-formatted-string', 'style-scope ytd-expander']
 var commentText = ''
+let port = chrome.runtime.connect({ name: "translate" });
 
 
-var port = chrome.runtime.connect({ name: "translate" });
 
-window.addEventListener('click',(event)=>{
+
+const makeButton = async () => {
+    await sleep(300)
+if (document.getElementById('center') != null) {
+    const header = document.getElementById('center')
+    console.log(header) // sees if the header is not null
+    const container = document.createElement('div');
+    const translator = document.createElement('div');
+    const pop = document.createElement('div');
+    container.className = "container";
+    translator.className = 'edge-effect';
+    translator.textContent = 'Translate'
+    pop.className = 'popup';
+    container.append(translator, pop);
+    header.appendChild(container);
+    console.log(header) //sees if the container is loaded with proper DOM elements
+}
+    document.querySelector('.edge-effect').addEventListener('click', function () {
+        var popup = document.querySelector('.popup');
+        console.log('running')
+        popup.classList.toggle('active');
+    });
+}
+
+function sleep(time){
+    return new Promise(resolve=>{
+        setTimeout(resolve(console.log(`you have waited ${time} milliseconds`)),time)
+    })
+}
+
+
+
+
+window.addEventListener('click', (event) => {
     console.log(event)
-    if (event.target.className.includes(array[0]) || event.target.className.includes(array[1])){
-        //console.log(event.target.innerHTML)
-        commentText = event.target.innerHTML
-        console.log('The inner text is ' + commentText)
+    if (event.target.className.includes(array[0]) ||
+        event.target.className.includes(array[1])) {
+        commentText = event.target.innerText
+        console.log(commentText)
         port.postMessage({ action: commentText });
     }
-    else
-        console.log(event.target.innerHTML)
 })
 
 
+function handleDisconnect() {
+    port.onDisconnect.addListener(async () => {
+        console.log("it disconnected again")
+        await sleep(1000);
+        port = null;
+        port = await chrome.runtime.connect({ name: "translate" });
+        console.log(port);
+        handleDisconnect(); 
+    });
+}
+
+handleDisconnect();
 
 
-
-
-
-//var port = chrome.runtime.connect({ name: "knockknock" });
-// port.postMessage({ joke: "Knock knock" });
-// port.onMessage.addListener(function (msg) {
-//     if (msg.question === "Who's there?"){
-//         port.postMessage({ answer: "Madame" });
-//         console.log(msg)
-//     }
-//     else if (msg.question === "Madame who?")
-//         port.postMessage({ answer: "Madame... Bovary" });
-// });
-
-console.log("Im here")
